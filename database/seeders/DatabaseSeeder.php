45124d7b23070a6a1e7d24b033258457e8afb337
Paulo Calueto Francisco <paulo.calueto.francisco@gmail.com>
2023-09-12 21:34:24 +0100

    category seed e factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            UserSeeder::class
-        ]);
+        $this->call(CategorySeeder::class);
     }
 }
