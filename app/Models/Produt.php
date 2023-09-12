4487f725601246ae4b319d17c93c83c7698d19d7
Paulo Calueto Francisco <paulo.calueto.francisco@gmail.com>
2023-09-12 20:48:18 +0100

    migrations produts e category

---
+++ app/Models/Produt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produt extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_09_12_172111_create_produts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produts', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->double('price',10,2);
+            $table->string('slug');
+            $table->string('image');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('id_category');
+            $table->foreign('id_category')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produts');
+    }
+};
